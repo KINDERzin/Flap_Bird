198a8a6be0da102cb449ba07f2a38ba2bd752a3f
Erick <erickfrias8006@gmail.com>
2024-10-10 11:49:47 -0300

    Pomba

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,12 +2,42 @@
 
 public partial class MainPage : ContentPage
 {
+	const int Gravidade = 2; // Pixel/Milisegundo
+	const int tempoEntreFrames = 25; // Milisegundos
+	bool estaMorto = true;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	
+	void AplicaGravidade()
+	{
+		imagemPersonagem.TranslationY += Gravidade;
+	}
+
+	async Task Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+
+	}
+
+	void OnGameOverClicked(object s, TappedEventArgs e)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenha();
+
+	}
+
+	void Inicializar()
+	{
+		estaMorto = false;
+		imagemPersonagem.TranslationY = 0;
+	}
 }
 
